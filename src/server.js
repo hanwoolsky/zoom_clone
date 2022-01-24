309364621216a8f7ec6a96f6c496340df778814e
hanwoolsky <hhw0925@naver.com>
2022-01-24 14:00:00 +0900

    [ADD]front&back socket.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,30 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
+
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app); // http server
+const wss = new WebSocket.Server({ server }); // webSocket server
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser✅");
+    socket.on("close", () => console.log("Disconnected from server❗"));
+    socket.on("message", message => {
+        console.log(message.toString());
+    });
+    socket.send("hello!");
+});
+
+
+server.listen(3000, handleListen);
