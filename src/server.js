9e40f837b208e8179d4fe33d0eda0e045edc00d8
hanwoolsky <hhw0925@naver.com>
2022-01-25 13:47:25 +0900

    [ADD]Chat func without frame.

--- src/server.js
+++ src/server.js
@@ -17,13 +17,26 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app); // http server
 const wss = new WebSocket.Server({ server }); // webSocket server
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+    sockets.push(socket); // 연결된 브라우저의 소켓을 저장
+    socket["nickname"] = "anonymouse";
     console.log("Connected to Browser✅");
     socket.on("close", () => console.log("Disconnected from server❗"));
-    socket.on("message", message => {
-        console.log(message.toString());
+    socket.on("message", msg => {
+        const message = JSON.parse(msg);
+        switch(message.type){
+            case "new_message":
+                sockets.forEach((aSocket) =>
+                    aSocket.send(`${socket.nickname}: ${message.payload.toString()}`)
+                );
+                break;
+            case "nickname":
+                socket["nickname"] = message.payload;
+                break;
+        }
     });
-    socket.send("hello!");
 });
 
 
